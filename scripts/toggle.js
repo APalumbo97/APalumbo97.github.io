9f6195c7b2b580045b7ca010bc0ded55d6c7d49b
Anthony Palumbo <ajpalumbo97@gmail.com>
2016-11-16 11:25:55 -0500

    Update toggle.js
    Added comments to toggle.js

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,3 +1,9 @@
+/**
+ * Author: Anthony Palumbo
+ * Date: 11-16-16
+ * Description: toggle function that animates the drop down menu for mobile displays.
+ */
+
 function toggle() {
     if(document.getElementById("tog").className.match(/(?:^|\s)showing(?!\S)/)) {
         document.getElementById("tog").className = "";
