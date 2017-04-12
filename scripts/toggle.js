13e7416297cf18bf33889db79da6e8a962ba4b8e
APalumbo97 <ajpalumbo97@gmail.com>
2017-04-12 16:43:29 -0400

    Fixed a typo.

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -5,10 +5,9 @@
  */
 
 function toggle() {
-    if(document.getElementById("tog").className.match(/(?:^|\s)showing(?!\S)/)) {
+    if (document.getElementById("tog").className.match(/(?:^|\s)showing(?!\S)/)) {
         document.getElementById("tog").className = "";
-    }
-    else {
+    } else {
         document.getElementById("tog").className = "showing";
     }
 }
