003ff4afad02570503df16dd9dc6da7c3fc3cfc8
APalumbo97 <ajpalumbo97@gmail.com>
2017-07-14 23:35:15 -0400

    Added email address on button click in connect page.

---
+++ scripts/showEmail.js
@@ -0,0 +1,12 @@
+/**
+ * Author: Anthony Palumbo
+ * Date: 7-14-2017
+ * Description: script to show email address after the user clicks a button.
+ */
+
+/**
+ * Inserts email address into a paragraph when called.
+ */
+function showEmail() {
+    document.getElementById("email").innerHTML = "ajpalumbo97@gmail.com";
+}
